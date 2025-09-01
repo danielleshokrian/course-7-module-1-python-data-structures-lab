5768af624c536c4a5462df537f06ac30d8d03798
Danielle <danielle.shokrian@aol.com>
2025-09-01 13:06:19 -0400

    Fixed format to pass tests for student_data

--- lib/data_processing.py
+++ lib/data_processing.py
@@ -1,7 +1,7 @@
 # This module contains functions to process student data.
 
 def format_student_data(student):
-    return f"ID: {student[0]}, Name: {student[1]}, Major: {student[2]}"
+    return f"ID: {student[0]} | Name: {student[1]} | Major: {student[2]}"
 
 def display_students(students):
     for student in students:
